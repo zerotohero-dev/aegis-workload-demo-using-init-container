ed6ad59868547c6d70800795be9a8cfd72907018
Volkan Özçelik <me@volkan.io>
2023-02-15 18:37:37 -0800

    intermediate

--- README.md
+++ README.md
@@ -8,10 +8,9 @@ keep your secrets… secret
 
 This is a demo Kubernetes deployment to test Aegis on a cluster.
 
-In this particular case, we leverage [**Aegis Go SDK**][aegis-sdk-go] instead 
-of using a sidecar and directly access **Aegis Safe**. Additionally, we leverage
-an **Aegis Init Container** to start the main container of the workload 
-only after a secret is  successfully registered to the workload.
+In this particular case, we leverage an **Aegis Init Container** to start 
+the main container of the workload only after a secret is  successfully 
+registered to the workload.
 
 [aegis-sdk-go]: https://github.com/zerotohero-dev/aegis-sdk-go
 [aegis-demo-sidecar]: https://github.com/zerotohero-dev/aegis-workload-demo-using-sidecar
--- main.go
+++ main.go
@@ -10,8 +10,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/zerotohero-dev/aegis-sdk-go/sentry"
-	"log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -32,26 +30,8 @@ func main() {
 	}()
 
 	for {
-		log.Println("fetch")
-		d, err := sentry.Fetch()
+		fmt.Printf("My secret: '%s'.", os.Getenv("SECRET"))
 
-		if err != nil {
-			fmt.Println("Failed to read the secrets file. Will retry in 5 seconds…")
-			fmt.Println(err.Error())
-			time.Sleep(5 * time.Second)
-			continue
-		}
-
-		if d.Data == "" {
-			fmt.Println("no secret yet… will check again later.")
-			time.Sleep(5 * time.Second)
-			continue
-		}
-
-		fmt.Printf(
-			"secret: updated: %s, created: %s, value: %s\n",
-			d.Updated, d.Created, d.Data,
-		)
 		time.Sleep(5 * time.Second)
 	}
 }
